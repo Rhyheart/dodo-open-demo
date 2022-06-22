ccb13341eab147f4040aafb0fb875b34bab19f0a
Rhyheart <8120183@qq.com>
2022-06-22 16:47:19 +0800

    improve dodo.open.info project

--- src/DoDo.Open.Info/Program.cs
+++ src/DoDo.Open.Info/Program.cs
@@ -20,75 +20,33 @@ var openApiService = new OpenApiService(new OpenApiOptions
     Token = appSetting.Token
 });
 
-string reply;
-
-#region 取群信息
-
-Console.WriteLine("[ 1、取群信息 ]\n");
-
-var getIslandInfoOutput = openApiService.GetIslandInfo(new GetIslandInfoInput
+try
 {
-    IslandId = appSetting.IslandId
-});
+    Console.WriteLine("\n\n[ 取群信息 ]\n");
 
-reply = "";
-reply += $"群号：{getIslandInfoOutput.IslandId}\n";
-reply += $"群名称：{getIslandInfoOutput.IslandName}\n";
-reply += $"群头像：{getIslandInfoOutput.CoverUrl}\n";
-reply += $"成员数：{getIslandInfoOutput.MemberCount}\n";
-reply += $"在线成员数：{getIslandInfoOutput.OnlineMemberCount}\n";
-reply += $"群描述：{getIslandInfoOutput.Description}";
-reply += $"默认进入频道：{getIslandInfoOutput.DefaultChannelId}\n";
-reply += $"系统通知频道：{getIslandInfoOutput.SystemChannelId}\n";
+    openApiService.GetIslandInfo(new GetIslandInfoInput
+    {
+        IslandId = appSetting.IslandId
+    }, true);
 
-Console.WriteLine(reply);
+    Console.WriteLine("\n[ 取频道列表 ]\n");
 
-#endregion
+    openApiService.GetChannelList(new GetChannelListInput
+    {
+        IslandId = appSetting.IslandId
+    }, true);
 
-#region 取频道列表
+    Console.WriteLine("\n[ 取身份组列表 ]\n");
 
-Console.WriteLine("[ 2、取频道列表 ]\n");
+    openApiService.GetRoleList(new GetRoleListInput
+    {
+        IslandId = appSetting.IslandId
+    }, true);
 
-var getChannelListOutput = openApiService.GetChannelList(new GetChannelListInput
+    Console.ReadKey();
+}
+catch (Exception e)
 {
-    IslandId = appSetting.IslandId
-});
-
-reply = "";
-
-foreach (var output in getChannelListOutput)
-{
-    reply += $"频道号：{output.ChannelId}\n";
-    reply += $"频道名称：{output.ChannelName}\n";
-    reply += $"频道类型：{output.ChannelType}\n";
-    reply += "\n";
+    Console.WriteLine("发生异常：" + e.Message);
 }
 
-Console.WriteLine(reply);
-
-#endregion
-
-#region 取身份组列表
-
-Console.WriteLine("[ 3、取身份组列表 ]\n");
-
-var getRoleListOutput = openApiService.GetRoleList(new GetRoleListInput
-{
-    IslandId = appSetting.IslandId
-});
-
-reply = "";
-
-foreach (var output in getRoleListOutput)
-{
-    reply += $"身份组名称：{output.RoleId}\n";
-    reply += $"身份组ID：{output.RoleName}\n";
-    reply += $"身份组颜色：{output.RoleColor}\n";
-    reply += "\n";
-}
-
-Console.WriteLine(reply); 
-
-#endregion
-
-Console.ReadKey();

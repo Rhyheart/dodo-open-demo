c30446e4ed61a75001989762fa3acc0c452a9c00
Rhyheart <8120183@qq.com>
2022-06-23 10:15:13 +0800

    add  test project

---
+++ src/DoDo.Open.Test/AppSetting.cs
@@ -0,0 +1,15 @@
+﻿namespace DoDo.Open.Test
+{
+    public class AppSetting
+    {
+        /// <summary>
+        /// 机器人唯一标识
+        /// </summary>
+        public string ClientId { get; set; }
+
+        /// <summary>
+        /// 机器人鉴权Token
+        /// </summary>
+        public string Token { get; set; }
+    }
+}
---
+++ src/DoDo.Open.Test/Program.cs
@@ -0,0 +1,30 @@
+﻿using DoDo.Open.Sdk.Models;
+using DoDo.Open.Sdk.Services;
+using DoDo.Open.Test;
+using Microsoft.Extensions.Configuration;
+
+//获取配置
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", false)
+    .Build();
+var appSetting = configuration.Get<AppSetting>();
+
+//接口服务
+var openApiService = new OpenApiService(new OpenApiOptions
+{
+    BaseApi = "https://botopen.imdodo.com",
+    ClientId = appSetting.ClientId,
+    Token = appSetting.Token
+});
+//事件处理服务，可自定义，只要继承EventProcessService抽象类即可
+var eventProcessService = new DemoEventProcessService(openApiService);
+//事件服务
+var openEventService = new OpenEventService(openApiService, eventProcessService, new OpenEventOptions
+{
+    IsReconnect = true,
+    IsAsync = true
+});
+//接收事件消息
+await openEventService.ReceiveAsync();
+
+Console.ReadKey();

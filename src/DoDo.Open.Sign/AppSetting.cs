56d75dce3c12918a4c8d54d91f70e7d3ee7e5464
Rhyheart <8120183@qq.com>
2022-07-01 15:00:28 +0800

    add sign project

---
+++ src/DoDo.Open.Sign/AppSetting.cs
@@ -0,0 +1,64 @@
+﻿namespace DoDo.Open.Sign
+{
+    public class AppSetting
+    {
+        /// <summary>
+        /// 机器人唯一标识
+        /// </summary>
+        public string ClientId { get; set; }
+
+        /// <summary>
+        /// 机器人鉴权Token
+        /// </summary>
+        public string Token { get; set; }
+
+        /// <summary>
+        /// 签到配置
+        /// </summary>
+        public Sign Sign { get; set; }
+
+        /// <summary>
+        /// 查询配置
+        /// </summary>
+        public Query Query { get; set; }
+
+        /// <summary>
+        /// 转账配置
+        /// </summary>
+        public Transfer Transfer { get; set; }
+    }
+
+    public class Sign
+    {
+        /// <summary>
+        /// 签到指令
+        /// </summary>
+        public string Command { get; set; }
+
+        /// <summary>
+        /// 获得积分
+        /// </summary>
+        public string Integral { get; set; }
+    }
+
+    public class Query
+    {
+        /// <summary>
+        /// 查询指令
+        /// </summary>
+        public string Command { get; set; }
+
+        /// <summary>
+        /// 签到回复
+        /// </summary>
+        public string Reply { get; set; }
+    }
+
+    public class Transfer
+    {
+        /// <summary>
+        /// 转账指令
+        /// </summary>
+        public string Command { get; set; }
+    }
+}
---
+++ src/DoDo.Open.Sign/BotEventProcessService.cs
@@ -0,0 +1,84 @@
+﻿using DoDo.Open.Sdk.Models.Channels;
+using DoDo.Open.Sdk.Models.Events;
+using DoDo.Open.Sdk.Models.Messages;
+using DoDo.Open.Sdk.Services;
+using Newtonsoft.Json;
+
+namespace DoDo.Open.Sign
+{
+    public class BotEventProcessService : EventProcessService
+    {
+        private readonly OpenApiService _openApiService;
+        private readonly AppSetting _appSetting;
+
+        public BotEventProcessService(OpenApiService openApiService, AppSetting appSetting)
+        {
+            _openApiService = openApiService;
+            _appSetting = appSetting;
+        }
+
+        public override void Connected(string message)
+        {
+            Console.WriteLine($"\n{message}\n");
+        }
+
+        public override void Disconnected(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public override void Reconnected(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public override void Exception(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public override void ChannelMessageEvent<T>(EventSubjectOutput<EventSubjectDataBusiness<EventBodyChannelMessage<T>>> input)
+        {
+            var eventBody = input.Data.EventBody;
+
+            if (eventBody.MessageBody is MessageBodyText messageBodyText)
+            {
+                var content = messageBodyText.Content.Replace(" ", "");
+                var reply = "";
+
+                var dataPath = $"{Environment.CurrentDirectory}\\data\\{eventBody.IslandId}.txt";
+
+                if (content == (_appSetting.Sign.Command))//签到
+                {
+                    var integral = DataHelper.GetValue<long>(dataPath, eventBody.DodoId, "Integral");
+                    Console.WriteLine(integral);
+                    integral++;
+                    DataHelper.SetValue(dataPath, eventBody.DodoId, "Integral", integral);
+                    reply = $"{integral}";
+                }
+                else if (content == (_appSetting.Query.Command))//查询
+                {
+
+                }
+                else if (content.StartsWith(_appSetting.Transfer.Command))//转账
+                {
+
+                }
+
+                if (reply != "")
+                {
+                    _openApiService.SetChannelMessageSend(new SetChannelMessageSendInput<MessageBodyText>
+                    {
+                        ChannelId = eventBody.ChannelId,
+                        MessageBody = new MessageBodyText
+                        {
+                            Content = reply
+                        }
+                    });
+                }
+
+            }
+
+        }
+    }
+}

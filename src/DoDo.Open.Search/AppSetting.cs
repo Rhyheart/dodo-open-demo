c000fc2327d486b10ff07b7433defc92cfc5bdda
Rhyheart <8120183@qq.com>
2022-07-05 14:51:25 +0800

    add search bot

---
+++ src/DoDo.Open.Search/AppSetting.cs
@@ -0,0 +1,34 @@
+﻿namespace DoDo.Open.Search
+{
+    public class AppSetting
+    {
+        /// <summary>
+        /// 机器人唯一标识
+        /// </summary>
+        public string ClientId { get; set; }
+
+        /// <summary>
+        /// 机器人鉴权Token
+        /// </summary>
+        public string Token { get; set; }
+
+        /// <summary>
+        /// 规则列表
+        /// </summary>
+        public List<Rule> RuleList { get; set; }
+    }
+
+    public class Rule
+    {
+        /// <summary>
+        /// 指令
+        /// </summary>
+        public string Command { get; set; }
+
+        /// <summary>
+        /// 回复
+        /// </summary>
+        public string Reply { get; set; }
+    }
+
+}
---
+++ src/DoDo.Open.Search/BotEventProcessService.cs
@@ -0,0 +1,80 @@
+﻿using System.Text.Encodings.Web;
+using System.Text.RegularExpressions;
+using DoDo.Open.Sdk.Models.Channels;
+using DoDo.Open.Sdk.Models.Events;
+using DoDo.Open.Sdk.Models.Messages;
+using DoDo.Open.Sdk.Services;
+
+namespace DoDo.Open.Search
+{
+    public class BotEventProcessService : EventProcessService
+    {
+        private readonly OpenApiService _openApiService;
+        private readonly AppSetting _appSetting;
+
+        public BotEventProcessService(OpenApiService openApiService, AppSetting appSetting)
+        {
+            _openApiService = openApiService;
+            _appSetting = appSetting;
+        }
+
+        public override void Connected(string message)
+        {
+            Console.WriteLine($"\n{message}\n");
+        }
+
+        public override void Disconnected(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public override void Reconnected(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public override void Exception(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public override void ChannelMessageEvent<T>(EventSubjectOutput<EventSubjectDataBusiness<EventBodyChannelMessage<T>>> input)
+        {
+            var eventBody = input.Data.EventBody;
+
+            #region 检索
+
+            if (eventBody.MessageBody is MessageBodyText messageBodyText)
+            {
+                var content = messageBodyText.Content.Replace(" ", "");
+                var defaultReply = $"<@!{eventBody.DodoId}>";
+                var reply = defaultReply;
+
+                var rule = _appSetting.RuleList.FirstOrDefault(x => Regex.IsMatch(content, $"{x.Command}(.*)"));
+                if (rule != null)
+                {
+                    var matchResult = Regex.Match(content, $"{rule.Command}(.*)");
+                    reply = rule.Reply
+                        .Replace("{DoDoId}",eventBody.DodoId)
+                        .Replace("{KeyWord}", UrlEncoder.Default.Encode(matchResult.Groups[1].Value));
+                }
+
+                #endregion
+
+                if (reply != defaultReply)
+                {
+                    _openApiService.SetChannelMessageSend(new SetChannelMessageSendInput<MessageBodyText>
+                    {
+                        ChannelId = eventBody.ChannelId,
+                        MessageBody = new MessageBodyText
+                        {
+                            Content = reply
+                        }
+                    });
+                }
+
+            }
+
+        }
+    }
+}

33e97f0291690cad3ac51f6429fb13ded527a202
Rhyheart <8120183@qq.com>
2022-12-12 15:08:00 +0800

    add ChatGPT project

---
+++ src/DoDo.Open.ChatGPT/AppSetting.cs
@@ -0,0 +1,34 @@
+﻿namespace DoDo.Open.ChatGPT
+{
+    public class AppSetting
+    {
+        /// <summary>
+        /// 机器人唯一标识
+        /// </summary>
+        public string ClientId { get; set; }
+
+        /// <summary>
+        /// 机器人鉴权Token
+        /// </summary>
+        public string Token { get; set; }
+
+        /// <summary>
+        /// 规则列表
+        /// </summary>
+        public List<Rule> RuleList { get; set; }
+    }
+
+    public class Rule
+    {
+        /// <summary>
+        /// 关键词，支持正则匹配
+        /// </summary>
+        public string KeyWord { get; set; }
+
+        /// <summary>
+        /// 回复
+        /// </summary>
+        public string Reply { get; set; }
+    }
+
+}
